5760884ffe259403058efd0e25b0292e8d89c379
Vijayaraagavan <vijayaraagavan.sadayappan@userflow.com>
2024-11-24 22:29:08 +0530

    added loader

--- src/pages/private/Products.jsx
+++ src/pages/private/Products.jsx
@@ -4,11 +4,14 @@ import { useBusinessInfo } from '/src/hooks/useBusinessContext';
 import { useState, useEffect } from 'react';
 import axios from '/src/utils/axios';
 import { PageContainer } from '/src/components/PageContainer.jsx';
+import Loader from '/src/components/Loader';
+
 function Products() {
   const businessInfo = useBusinessInfo();
   const [products, setProducts] = useState([]);
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     if (!businessInfo.orgId) return;
     setLoading(true);
@@ -81,6 +84,7 @@ function Products() {
 
   return (
     <PageContainer subtitle="Products" title="Products">
+      {loading && <Loader></Loader>}
       <div className="min-h-screen bg-gray-50 p-8">
         <div className="max-w-7xl mx-auto">
           <CustomTable columns={columns} data={data} setData={setData} />
